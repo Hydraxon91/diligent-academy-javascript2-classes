ce695993e02441aa2afac231a65a0a99711b186a
manobalaban <mano.fabian@codecool.com>
2024-10-03 15:39:38 +0200

    for demo without tasks

---
+++ 0_demo/higher_order_functions.js
@@ -0,0 +1,40 @@
+const numbers = [33, 3, 4, 25, 12, 44, 9];
+
+const cars = ['opel', 'bmw', 'skoda', 'ferrari', 'dacia']
+
+const persons = [
+    {
+        name: 'John',
+        age: 21
+    },
+    {
+        name: 'Bob',
+        age: 50
+    },
+    {
+        name: 'Frank',
+        age: 34
+    },
+    {
+        name: 'James',
+        age: 50
+    }
+];
+
+//forEach
+
+//map
+
+//find
+
+//findIndex
+
+//filter
+
+//some
+
+//every
+
+//sort
+
+//reduce
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# diligent-academy-javascript2-classes
+# Classes
+
+## Background material
+
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+- https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Inheritance_and_the_prototype_chain
